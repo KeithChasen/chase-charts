5028c24c12d033435340c6b696159bd015492215
keithchasen <keithchasen89@gmail.com>
2025-07-14 23:15:37 +0200

    basic x/y plot added and drawned a line on the canvas

--- demo/App.tsx
+++ demo/App.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import { Chart } from 'chase-charts';
 
 export const App = () => {
+	const testData = [
+		{ x: 10, y: 20 },
+		{ x: 15, y: 30 },
+		{ x: 20, y: 40 },
+		{ x: 25, y: 50 },
+		{ x: 30, y: 60 },
+		{ x: 35, y: 70 },
+		{ x: 40, y: 80 },
+		{ x: 45, y: 90 },
+		{ x: 50, y: 100 },
+		{ x: 55, y: 110 },
+	];
+
 	return (
 		<div>
 			<h1>Charts Demo</h1>
-			<Chart />
+			<Chart data={testData} />
 		</div>
 	);
 };
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-export const Chart = () => {
-	return <canvas></canvas>;
+export const Chart = ({ data }: { data: { x: number; y: number }[] }) => {
+	const ref = useRef<HTMLCanvasElement | null>(null);
+
+	const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+	const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+
+	useEffect(() => {
+		if (ref.current) {
+			setCanvas(ref.current);
+			setCtx(ref.current.getContext('2d'));
+		}
+	}, [ref.current]);
+
+	useEffect(() => {
+		if (canvas && ctx) {
+			if (data.length) {
+				ctx.beginPath();
+				for (let i = 0; i < data.length; i++) {
+					if (i === 0) {
+						ctx.moveTo(data[i].x, data[i].y);
+					} else {
+						ctx.lineTo(data[i].x, data[i].y);
+					}
+				}
+				ctx.stroke();
+			}
+		}
+	}, [canvas, ctx]);
+
+	return (
+		<canvas
+			ref={ref}
+			style={{
+				width: '100%',
+				height: '100%',
+			}}
+		></canvas>
+	);
 };
 
 export default Chart;
